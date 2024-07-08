6e45b9f7fe7f774cb0b32285f63fdd516ccab08e
Kirill <kirillgubarev5667@gmail.com>
2024-07-08 19:59:08 +0700

    linked glfw lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
+#pragma comment (lib, "opengl32.lib")
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main(int argc, char *argv[]){
-	std::cout<<"Hello monkey"<<std::endl;
+    if (!glfwInit())
+        return -1;
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glClearColor(1, 1, 0, 1);
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
 	system("pause");
 	return 0;
 }
